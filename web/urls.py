fbb4918b705875138cc05ced9c22f54f75fa9907
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2025-04-04 15:03:44 -0300

    Se agregan templates dinámicos.
    Se agregaron templates dinámicos para mostrar categorias en subcategorias.html y detalle de los juegos en detalle.html
    
    Los datos se pasan a views.py en dos diccionarios, categorias y juegos.
    
    Se eliminan antiguos archivos html y carpetas innecesarias.

--- web/urls.py
+++ web/urls.py
@@ -11,31 +11,12 @@ urlpatterns = [
     path('contacto/', views.contacto, name='contacto'),
     path('carrito/', views.carrito, name='carrito'),
 
-    # Rutas para las categorías
-    path('categorias/carreras/', views.categoria_carreras, name='categoria_carreras'),
-    path('categorias/cozy/', views.categoria_cozy, name='categoria_cozy'),
-    path('categorias/mundo_abierto/', views.categoria_mundoabierto, name='categoria_mundoabierto'),
-    path('categorias/shooters/', views.categoria_shooters, name='categoria_shooters'), 
-    path('categorias/terror/', views.categoria_terror, name='categoria_terror'),
+    # Ruta dinámica para las categorías
+    path('categoria/<int:categoria_id>/', views.subcategoria, name='subcategoria'),
 
-    # Rutas para los juegos - Carreras
-    path('juegos/carreras/crashteamracing/', views.crashteamracing, name='crashteamracing'),
-    path('juegos/carreras/mariokart8deluxe/', views.mariokart8deluxe, name='mariokart8deluxe'),
+   # Ruta dinámica para juegos
+    path("juego/<int:juego_id>/", views.detalle_juego, name="detalle_juego"),
 
-    # Juegos - Cozy
-    path('juegos/cozy/stardewvalley/', views.stardewvalley, name='stardewvalley'),
-    path('juegos/cozy/spiritfarer/', views.spiritfarer, name='spiritfarer'),
 
-    # Juegos - Mundo Abierto
-    path('juegos/mundoabierto/cyberpunk2077/', views.cyberpunk2077, name='cyberpunk2077'),
-    path('juegos/mundoabierto/zeldatotk/', views.zeldatotk, name='zeldatotk'),
-
-    # Juegos - Shooters
-    path('juegos/shooters/doometernal/', views.doometernal, name='doometernal'),
-    path('juegos/shooters/splatoon2/', views.splatoon2, name='splatoon2'),
-
-    # Juegos - Terror
-    path('juegos/terror/alanwake2/', views.alanwake2, name='alanwake2'),
-    path('juegos/terror/residentevil4remake/', views.residentevil4remake, name='residentevil4remake'),
 ]
 
