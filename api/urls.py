abb10c966efc54069bbcc5d0fd43c33093913015
maxi <maxi.negrete@duocuc.cl>
2025-04-23 18:36:53 -0400

    integracion de API externa de videojuegos completada

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CategoriaListAPIView
+from .views import CategoriaListAPIView, juegos_populares
 
 urlpatterns = [
     path('categorias/', CategoriaListAPIView.as_view(), name='api_categorias'),
+    path('juegos-populares/', juegos_populares, name='juegos_populares'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,18 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from core.models import Categoria
 from .serializers import CategoriaSerializer
+from .external import obtener_juegos_populares
 
 class CategoriaListAPIView(generics.ListAPIView):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+@api_view(['GET'])
+def juegos_populares(request):
+    """Devuelve una lista de juegos populares desde la API RAWG"""
+    juegos = obtener_juegos_populares()
+    return Response(juegos)
+
 
